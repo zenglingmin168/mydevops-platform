ad4d5d3b5743891d9c4fe0474dba35e895ffebb5
zenglingmin168 <44182930+zenglingmin168@users.noreply.github.com>
2023-07-30 09:34:02 +0400

    Update flush_gateway.sh

--- stu_shop_managerBE/flush_gateway.sh
+++ stu_shop_managerBE/flush_gateway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ssh serveradm@172.16.16.83 -p1876 -i /Users/zenglingmin/Downloads/qcloud_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.48.154/actuator/refresh"'
-ssh serveradm@172.16.16.83 -p1876 -i /Users/zenglingmin/Downloads/qcloud_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.157.150/actuator/refresh"'
-ssh serveradm@172.16.16.83 -p1876 -i /Users/zenglingmin/Downloads/qcloud_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.233.122/actuator/refresh"'
-ssh serveradm@172.16.16.83 -p1876 -i /Users/zenglingmin/Downloads/qcloud_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.217.9/actuator/refresh"'
-ssh serveradm@172.16.16.83 -p1876 -i /Users/zenglingmin/Downloads/qcloud_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.48.158/actuator/refresh"'
-ssh serveradm@172.16.16.83 -p1876 -i /Users/zenglingmin/Downloads/qcloud_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.169.190/actuator/refresh"'
+ssh serveradm@172.116.116.83 -p1876 -i /Users/zenglingmin/Downloads/qc_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.48.154/actuator/refresh"'
+ssh serveradm@172.116.116.83 -p1876 -i /Users/zenglingmin/Downloads/qc_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.157.150/actuator/refresh"'
+ssh serveradm@172.116.116.83 -p1876 -i /Users/zenglingmin/Downloads/qc_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.233.122/actuator/refresh"'
+ssh serveradm@172.116.116.83 -p1876 -i /Users/zenglingmin/Downloads/qc_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.217.9/actuator/refresh"'
+ssh serveradm@172.116.116.83 -p1876 -i /Users/zenglingmin/Downloads/qc_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.48.158/actuator/refresh"'
+ssh serveradm@172.116.116.83 -p1876 -i /Users/zenglingmin/Downloads/qc_serveradm_default_new.key 'curl -o /dev/null -s -H "Content-Type: application/json" -X POST  "http://10.83.169.190/actuator/refresh"'
